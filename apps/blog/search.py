f42f3428096369128e56f85af00007a475fa80c8
Irina <irinakhaus@gmail.com>
2023-09-09 13:17:11 +0300

    Refactoring code

--- apps/blog/search.py
+++ apps/blog/search.py
@@ -1,17 +1,16 @@
-# from elasticsearch_dsl.query import Q, MultiMatch
-# from .documents import ArticleDocument
+from elasticsearch_dsl.query import Q, MultiMatch
+from .documents import ArticleDocument
 
 
-# def get_search_query(phrase: str) -> ArticleDocument:
-#     query = Q(
-#         'function_score',
-#         query=MultiMatch(
-#             fields=['title', 'body'], query=phrase
-#         ),
-#     )
-#     return ArticleDocument.search().query(query)
+def get_search_query(phrase: str) -> ArticleDocument:
+    query = Q(
+        'function_score',
+        query=MultiMatch(
+            fields=['title', 'body'], query=phrase
+        ),
+    )
+    return ArticleDocument.search().query(query)
 
 
 def search(phrase: str):
-    return
-    # return get_search_query(phrase).to_queryset()
+    return get_search_query(phrase).to_queryset()
