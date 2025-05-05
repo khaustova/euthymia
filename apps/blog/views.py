f4d46f649febb31b0189c4d47a7b18764a5f3872
Irina Khaustova <irinakhaus@gmail.com>
2025-05-05 23:53:59 +0300

    Update & Style pagination

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -24,8 +24,8 @@ class ArticleView(ListView):
     model = Article
     template_name = 'blog/index.html'
     context_object_name = 'articles'
-    paginate_by = 6
-    paginate_orphans = 3
+    paginate_by = 10
+    paginate_orphans = 5
 
     def get_queryset(self):
         """
