1015657f3a429c655fc599adab13b081e2e564c5
Irina Khaustova <irinakhaus@gmail.com>
2026-01-06 01:34:53 +0300

    Add comments & Register SiteSettings model

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,33 +1,70 @@
 from admin_auto_filters.filters import AutocompleteFilter
-from django.db import transaction
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.urls import reverse
 from mptt.admin import DraggableMPTTAdmin
-from apps.manager.tasks import send_notification
-from .models import Article, Category, Comment, UserProfile, Subcategory
+from .models import Article, Category, Comment, UserProfile, Subcategory, SiteSettings
 
 
 class CategoryFilter(AutocompleteFilter):
+    """
+    Фильтр категорий с автодополнением.
+    """
     title = 'Категория'
     field_name = 'category'
-    
+
+
 class SubcategoryFilter(AutocompleteFilter):
+    """
+    Фильтр подкатегорий с автодополнением.
+    """
     title = 'Подкатегория'
     field_name = 'subcategory'
-    
-    
+
+
 class ArticleFilter(AutocompleteFilter):
+    """
+    Фильтр статей с автодополнением.
+    """
     title = 'Статья'
     field_name = 'article'
+    
+    
+@admin.register(SiteSettings)
+class SiteDescriptionAdmin(admin.ModelAdmin):
+    """
+    Настройки администрирования модели настроек сайта SiteSettings.
+
+    Реализует паттерн синглтон:
+    - если объект не существует, то открывается форма создания;
+    - если объект существует, то открывается его форма редактирования.
+    """
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        first_obj = self.model.objects.first()
+        if first_obj is not None:
+            return redirect(reverse('admin:blog_sitesettings_change', args=(first_obj.pk,)))
+        return redirect(reverse('admin:blog_sitesettings_add'))
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    """
+    Настройки администрирования модели статьи Article.
+
+    Особенности:
+    - поля формы редактирования статьи;
+    - отображаемые колонки в списке;
+    - фильтр по категории и подкатегории;
+    - автогенерация слага;
+    - поиск по заголовку.
+    """
     fields = (
         'title', 
-        'slug', 
-        'image',
+        'slug',
         'summary', 
         'body', 
         'category', 
@@ -45,51 +82,55 @@ class ArticleAdmin(admin.ModelAdmin):
         'status',
     )
     list_display_links = ('title',)
-    list_filter = [
-        'status',
-        CategoryFilter,
-        SubcategoryFilter,
-    ]
-    prepopulated_fields = {
-       'slug': ('title',)
-    }
-    list_per_page = 50
+    list_filter = ['status', CategoryFilter, SubcategoryFilter]
+    prepopulated_fields = {'slug': ('title',)}
+    list_per_page = 100
     date_hierarchy = 'created_date'
-    search_fields = (
-        'title',
-    )
+    search_fields = ('title',)
 
     change_form_template = 'dashboard/change_form_article.html'
-
-    def save_model(self, request, obj, form, change):
-        super().save_model(request, obj, form, change)
-        if not change:
-            transaction.on_commit(lambda: send_notification.delay(obj.pk))
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    """
+    Настройки администрирования модели категории Category.
+
+    Особенности:
+    - поиск категории по названию;
+    - автогенерация слага.
+    """
     list_display = ('name', 'type')
     search_fields = ('name',)
-    prepopulated_fields = {
-        'slug': ('name',)
-    }
-    list_filter = [
-        'type',
-    ]
-    
-    
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ['type',]
+
+
 @admin.register(Subcategory)
 class SubcategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category',)
+    """
+    Настройки администрирования модели подкатегории Subcategory.
+
+    Особенности:
+    - поиск подкатегории по названию;
+    - фильтр по категории;
+    - автогенерация слага.
+    """
+    list_display = ('name', 'category')
     search_fields = ('name',)
-    list_filter = [
-        CategoryFilter,
-    ]
+    list_filter = [CategoryFilter,]
 
 
 @admin.register(Comment)
 class CommentAdmin(DraggableMPTTAdmin):
+    """
+    Настройки администрирования модели комментариев Comment.
+
+    Особенности:
+    - вложенные комментарии;
+    - перетаскивание для изменения иерархии;
+    - фильтр по дате создания и статье.
+    """
     list_display = (
         'tree_actions', 
         'indented_title', 
@@ -99,22 +140,27 @@ class CommentAdmin(DraggableMPTTAdmin):
         'created_date'
     )
     mptt_level_indent = 2
-    list_display_links = (
-        'article',
-    )
-    list_filter = [
-        'created_date',
-        ArticleFilter,
-    ]
+    list_display_links = ('article',)
+    list_filter = ['created_date', ArticleFilter]
 
 
 class UserInline(admin.StackedInline):
+    """
+    Модель для отображения и редактирования профиля пользователя
+    непосредственно в административной форме пользователя.
+    """
     model = UserProfile
     can_delete = False
     verbose_name_plural = 'Дополнительная информация'
 
 
 class UserAdmin(UserAdmin):
+    """
+    Расширенная административная панель пользователя.
+
+    Добавляет связанную модель UserProfile
+    в форму редактирования стандартного пользователя.
+    """
     inlines = (UserInline, )
 
 
